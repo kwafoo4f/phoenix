54f574c560f1b86baf52c64b563daf12db62c68d
zhongkai <1980274824@qq.com>
2023-09-09 13:13:32 +0800

    server-init

---
+++ server/src/main/java/com/kuafoo4f/phoenix/ServerApp.java
@@ -0,0 +1,16 @@
+package com.kuafoo4f.phoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: zk
+ * @date: 2023-09-09 12:41
+ */
+@SpringBootApplication
+public class ServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApp.class,args);
+    }
+}
