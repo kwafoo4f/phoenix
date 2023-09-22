357a1bab891635873a840f32efa9d905a652905c
zhongkai <1980274824@qq.com>
2023-09-22 09:52:15 +0800

    客户端服务注册和服务心跳,测试

--- example/order-service/src/main/java/com/kufoo4j/service/ServiceController.java
+++ example/order-service/src/main/java/com/kufoo4j/service/ServiceController.java
@@ -3,6 +3,7 @@ package com.kufoo4j.service;
 import com.kuafoo4j.phoenix.client.ServiceHolder;
 import com.kuafoo4j.phoenix.commom.core.api.ReturnResp;
 import com.kuafoo4j.phoenix.commom.core.pojo.ServiceInfo;
+import com.kuafoo4j.phoenix.config.ServiceProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServiceController {
     @Autowired
     private ServiceHolder serviceHolder;
+    @Autowired
+    private ServiceProperties serviceProperties;
 
     @GetMapping
     public ReturnResp getServiceInfo(@RequestParam String serviceName) {
         ServiceInfo serviceInfo = serviceHolder.getAllInstance(serviceName);
         return ReturnResp.ok(serviceInfo);
+    }
+
+    @GetMapping("/server-info")
+    public String getServerInfo() {
+        StringBuffer serverInfo = new StringBuffer()
+                .append(serviceProperties.getServiceName()).append("@")
+                .append(serviceProperties.getIp())
+                .append(":").append(serviceProperties.getPort());
+        return serverInfo.toString();
     }
 
     @GetMapping("/all")
